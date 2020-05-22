c00f1c69b5456ce2819df525e6fb99c86d2d9d97
DanceParty <keevandance@gmail.com>
2020-05-22 15:53:36 -0700

    Initial Commit

---
+++ appcenter-testapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  appcenter-testapp
+//
+//  Created by Keevan Dance on 5/22/20.
+//  Copyright © 2020 Keevan Dance. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
